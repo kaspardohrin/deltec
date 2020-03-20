02f17d20ddd838af10b05199409379e74b921fcb
Kaspar <0882916@hr.nl>
2020-03-20 12:12:22 +0100

    Styling, iframe, and crispness

---
+++ user-interface-plain/style.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    document.getElementsByTagName('iframe')[0].setAttribute("style", "display: none;")
+})
+
+function iframe() {
+    document.getElementsByTagName('iframe')[0].setAttribute("style", "position: absolute; border: 1px solid black; background-color: white; width: 100%; height: 600px; left: 0; top: 59.25px;")
+}
