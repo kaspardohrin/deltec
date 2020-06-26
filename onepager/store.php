2272efc1b4f6af3e732753c9031ca1b685d4c72a
Rovexx <roel.versteeg@hotmail.com>
2020-06-26 11:31:06 +0200

    Converted to Bootstrap
    Changed images and converted everything to bootstrap in stead of materialize

---
+++ onepager/store.php
@@ -0,0 +1,180 @@
+<?php include_once 'includes/head.html';?>
+  <body>
+    <!-- Include navbar -->
+    <?php include_once 'includes/navbar.html';?>
+
+    <!-- Logo -->
+    <div class="container">
+      <div class="row">
+        <div class="col text-center mb-3">
+          <img src="content/logos/Deltec.png" class="img-fluid rounded" alt="Responsive image">
+          <h1>De persoonlijke telepresence robot</h1>
+        </div>
+      </div>
+    </div>
+
+    <!-- Versies -->
+    <div class="bg-blue">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center">
+            <h1 class="font-weight-bold text-light mt-3">COMPLETE MODELLEN</h1>
+            <h5>Welke samenstelling is geschikt voor jou?</h5>
+          </div>
+        </div>
+        <div class="row justify-content-center pb-3">
+          <!-- Budget -->
+          <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+            <h2 class="my-2 font-weight-bold text-center">Budget</h2>
+            <img src="content/robot/Deltec_budget.png" class="img-fluid rounded">
+            <p class="mt-3">
+              <span class="font-weight-bold">€30,- | Instap model</span><br>
+              Maak kennis met het Deltec ecosysteem zonder de bank te breken.
+              Er ontbreken misschien wielen maar deze kun je er altijd bij kopen.
+            </p>
+            <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark align-items-end" href="/store.php">SHOP VERDER
+              <i class="material-icons align-middle">more_horiz</i>
+            </a>
+          </div>
+          <!-- Belegger -->
+          <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+            <h2 class="my-2 font-weight-bold text-center">Belegger</h2>
+            <img src="content/robot/Deltec_wielen_2_transparant.png" class="img-fluid rounded">
+            <p class="mt-3">
+              <span class="font-weight-bold">€50,- | Gevorderd model</span><br>
+              Je wilt een Deltec robot maar dan wel wat betere modules om hem wat meer karakter te geven.<br>
+              Maar pas op waar je rijdt!
+            </p>
+            <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark" href="/store.php">SHOP VERDER
+              <i class="material-icons align-middle">more_horiz</i>
+            </a>
+          </div>
+          <!-- Collector -->
+          <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+            <h2 class="my-2 font-weight-bold text-center">Collector</h2>
+            <img src="content/robot/Deltec_wielen_3_transparant.png" class="img-fluid rounded">
+            <p class="mt-3">
+              <span class="font-weight-bold">€100,- | Alles erop en eraan</span><br>
+              Met deze kit kom je niks tekort. 
+              Alle mogelijke onderdelen voor een zo divers mogelijke robot. 
+              Hiermee kun je alle features uitproberen.
+            </p>
+            <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark" href="/store.php">SHOP VERDER
+              <i class="material-icons align-middle">more_horiz</i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Onderdelen -->
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <h1 class="font-weight-bold text-blue mt-3">MODULES</h1>
+          <h5>Robot customizen of onderdelen vervangen?</h5>
+        </div>
+      </div>
+      <!-- Wielen -->
+      <div class="row justify-content-center pb-3">
+        <!-- Wielen 1 -->
+        <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+          <h2 class="my-2 font-weight-bold text-center">Wielen | Geen</h2>
+          <img src="content/robot/Deltec_budget.png" class="img-fluid rounded">
+          <p class="mt-3">
+            <span class="font-weight-bold">€30,- | Instap model</span><br>
+            Maak kennis met het Deltec ecosysteem zonder de bank te breken.
+            Er ontbreken misschien wielen maar deze kun je er altijd bij kopen.
+          </p>
+          <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark align-items-end" href="/store.php">SHOP VERDER
+            <i class="material-icons align-middle">more_horiz</i>
+          </a>
+        </div>
+        <!-- Wielen 2 -->
+        <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+          <h2 class="my-2 font-weight-bold text-center">Wielen | Standaard</h2>
+          <img src="content/robot/Deltec_wielen_2_transparant.png" class="img-fluid rounded">
+          <p class="mt-3">
+            <span class="font-weight-bold">€50,- | Gevorderd model</span><br>
+            Je wilt een Deltec robot maar dan wel wat betere modules om hem wat meer karakter te geven.<br>
+            Maar pas op waar je rijdt!
+          </p>
+          <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark" href="/store.php">SHOP VERDER
+            <i class="material-icons align-middle">more_horiz</i>
+          </a>
+        </div>
+        <!-- Wielen 3 -->
+        <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+          <h2 class="my-2 font-weight-bold text-center">Wielen | Rupsbanden</h2>
+          <img src="content/robot/Deltec_wielen_3_transparant.png" class="img-fluid rounded">
+          <p class="mt-3">
+            <span class="font-weight-bold">€100,- | Alles erop en eraan</span><br>
+            Met deze kit kom je niks tekort. 
+            Alle mogelijke onderdelen voor een zo divers mogelijke robot. 
+            Hiermee kun je alle features uitproberen.
+          </p>
+          <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark" href="/store.php">SHOP VERDER
+            <i class="material-icons align-middle">more_horiz</i>
+          </a>
+        </div>
+      </div>
+      <!-- Ogen -->
+      <div class="row justify-content-center pb-3">
+        <!-- RGB led -->
+        <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+          <h2 class="my-2 font-weight-bold text-center">Ogen | RGB Leds</h2>
+          <img src="content/robot/Deltec_budget.png" class="img-fluid rounded">
+          <p class="mt-3">
+            <span class="font-weight-bold">€30,- | Instap model</span><br>
+            Maak kennis met het Deltec ecosysteem zonder de bank te breken.
+            Er ontbreken misschien wielen maar deze kun je er altijd bij kopen.
+          </p>
+          <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark align-items-end" href="/store.php">SHOP VERDER
+            <i class="material-icons align-middle">more_horiz</i>
+          </a>
+        </div>
+        <!-- 7 Segment -->
+        <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+          <h2 class="my-2 font-weight-bold text-center">Ogen | 7 Segment</h2>
+          <img src="content/robot/Deltec_wielen_2_transparant.png" class="img-fluid rounded">
+          <p class="mt-3">
+            <span class="font-weight-bold">€50,- | Gevorderd model</span><br>
+            Je wilt een Deltec robot maar dan wel wat betere modules om hem wat meer karakter te geven.<br>
+            Maar pas op waar je rijdt!
+          </p>
+          <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark" href="/store.php">SHOP VERDER
+            <i class="material-icons align-middle">more_horiz</i>
+          </a>
+        </div>
+        <!-- 8x8 Matrix -->
+        <div class="col-md-5 col-xl m-1 mb-3 bg-white shadow infobox">
+          <h2 class="my-2 font-weight-bold text-center">Ogen | 8x8 Matrix</h2>
+          <img src="content/robot/Deltec_wielen_3_transparant.png" class="img-fluid rounded">
+          <p class="mt-3">
+            <span class="font-weight-bold">€100,- | Alles erop en eraan</span><br>
+            Met deze kit kom je niks tekort. 
+            Alle mogelijke onderdelen voor een zo divers mogelijke robot. 
+            Hiermee kun je alle features uitproberen.
+          </p>
+          <a type="button" class="btn btn-outline-primary btn-lg btn-block mb-3 text-dark" href="/store.php">SHOP VERDER
+            <i class="material-icons align-middle">more_horiz</i>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-grey">
+      <br>
+      <!-- Include footer -->
+      <?php include_once 'includes/footer.html';?>
+    </div>
+    <!-- Include scripts -->
+    <?php include_once 'includes/scripts.html';?>
+    <script>
+      $(document).ready(function () {
+        $('#nav_home').removeClass('active');
+        $('#nav_shop').addClass('active');
+      });
+    </script>
+  </body>
+</html>
